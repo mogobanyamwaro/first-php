f84c82053f10317f76b1c163cbc45a593f5606c0
mogobanyamwaro <mogobanyamwaro@gmail.com>
2021-09-29 13:44:22 +0300

    initial commit

---
+++ practise.php
@@ -0,0 +1,25 @@
+<?php
+$a = 10;
+$b = 20;
+if($a > $b){
+    echo "a is greater than b";
+}
+elseif($b ==$a){
+    echo"b is greater than a";
+}
+else{
+    echo"hallooo";
+}
+echo "<br>";
+$index_array = ['mogoba','douglas'];
+echo $index_array[0];
+$associative_array = ['pencils'=>10,'erasers'=>3];
+echo $associative_array['pencils'];
+
+for ($i=0; $i < 10; $i++) { 
+    # code...
+    echo $i;
+}
+
+
+?>
